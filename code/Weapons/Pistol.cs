7ed2cb62e72c9ddc89e56df5ab49ccf28c655a26
Ephememory <leetvipertm@gmail.com>
2022-04-12 10:33:39 -0500

    Remove leftover pistol impact discharge code

--- code/Weapons/Pistol.cs
+++ code/Weapons/Pistol.cs
@@ -45,32 +45,6 @@ partial class Pistol : Weapon
 		ShootBullet( 0.05f, 1.5f, 9.0f, 3.0f );
 	}
 
-	private void Discharge()
-	{
-		if ( TimeSinceDischarge < 0.5f )
-			return;
-
-		TimeSinceDischarge = 0;
-
-		var muzzle = GetAttachment( "muzzle" ) ?? default;
-		var pos = muzzle.Position;
-		var rot = muzzle.Rotation;
-
-		ShootEffects();
-		PlaySound( "rust_pistol.shoot" );
-		ShootBullet( pos, rot.Forward, 0.05f, 1.5f, 9.0f, 3.0f );
-
-		ApplyAbsoluteImpulse( rot.Backward * 200.0f );
-	}
-
-	protected override void OnPhysicsCollision( CollisionEventData eventData )
-	{
-		if ( eventData.Speed > 500.0f )
-		{
-			Discharge();
-		}
-	}
-
 	public override void SimulateAnimator( PawnAnimator anim )
 	{
 		anim.SetAnimParameter( "holdtype", 1 );
