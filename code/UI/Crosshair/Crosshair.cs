6126295b46ea258c4d67b98f8b33b8920eeb1028
Ephememory <leetvipertm@gmail.com>
2022-04-12 06:52:13 -0500

    Add new crosshair
    crosshair html + scss

---
+++ code/UI/Crosshair/Crosshair.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using Sandbox;
+using Sandbox.UI;
+
+namespace Breakfloor.UI
+{
+	[UseTemplate]
+	public partial class Crosshair : Panel
+	{
+		public Crosshair()
+		{
+		}
+	}
+}
